0fecef386dd6d1a03e85ec32da28def55a18f76c
Alice1360 <72809385+Alice1360@users.noreply.github.com>
2021-01-25 22:12:03 +0100

    Exo2-Alice

--- 2-superglobales.php
+++ 2-superglobales.php
@@ -1,10 +1,44 @@
-<form action="">
+<form action=""
+ method="post">
     <div>
-        <label for="test">Un champ de test</label>
-        <input type="text" id="test">
+        <label for="test">Quantité</label>
+        <input type="number" id="test" name="quantity">
     </div>
     <div>
-        <label for="email">Un champ email</label>
-        <input type="email" id="email">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
     </div>
+    <button type="submit">Ajouter</button>
 </form>
+
+<?php
+session_start();
+
+$stock = $_GET['stock'];
+$errors = [];
+
+if (empty($stock)) {
+    header('locatio: 1-html.php');
+}
+$_SESSION['stock'] = $stock;
+
+ if (isset($_POST['quantity'])) {
+     if ($_POST['quantity'] > $stock) {
+         $errors[] = "Il n'y a pas assez de stock pour répondre à votre demande";
+     }
+ }
+
+if (isset($_POST['email'])) {
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors [] = 'Veuillez entrer un email valide';
+    }
+}
+ if (isset($_POST['email']) && isset($_POST['quantity']) && empty($errors)) {
+     $_SESSION['email'] = $_POST['email'];
+     $_SESSION['quantity'] = $_POST['quantity'];
+     var_dump($_SESSION);
+ }
+
+ foreach ($errors as $error) {
+     echo $error. '</br>';
+ }
