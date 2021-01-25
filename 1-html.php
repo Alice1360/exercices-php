ecd6e0ecce98cae8311e23cca32336fa93d4eef8
Alice1360 <72809385+Alice1360@users.noreply.github.com>
2021-01-25 14:09:37 +0100

    Exo-1-Alice

--- 1-html.php
+++ 1-html.php
@@ -27,3 +27,39 @@ $shoes = [
         'stock' => 32,
     ],
 ];
+
+foreach ($shoes as $shoe) {
+    ?>
+
+    <table>
+        <th>Noms</th>
+        <th>Prix</th>
+        <th>Prix si soldé</th>
+        <th>Quantité</th>
+        <th>Produit</th>
+        <tr style="color:<?php
+                if ($shoe['stock']< 20) {
+                    echo 'red';
+                } ?>">
+            <td><?= $shoe['name'] ?></td>
+            <td><?= $shoe['price']; ?> €</td>
+            <td> <?php
+    if ($shoe['price'] > 100) {
+        $newPrice = sales($shoe['price'], 10);
+        echo "$newPrice €";
+    } else {
+        echo $shoe['price'];
+        echo "€";
+    } ?></td>
+            <td><?= $shoe['stock'] ?></td>
+            <td><a href="2-superglobales.php?stock=<?php echo $shoe['stock']; ?>
+"><?= $shoe['name']; ?></a></td>
+        </tr>
+    </table>
+    <?php
+}
+
+function sales(float $price, float $discount)
+{
+    return number_format(($price - ($price * $discount)/100), 2, ",", " ");
+}
