fe82de40187708c8fca1e4f3d439f73b413151ed
Alice1360 <72809385+Alice1360@users.noreply.github.com>
2021-01-25 22:51:04 +0100

    exo4-Alice

---
+++ 4-debug.php
@@ -0,0 +1,42 @@
+<?php
+//
+//
+// Corriger les erreurs et les mauvaises pratiques dans l'extrait de code suivant
+//
+//
+    $variable = $_GET['variable'];
+    if (isset($variable)) {
+        echo $variable;
+    }
+    function UneFonction(int $parametre): int
+    {
+        return $parametre * 2;
+    }
+
+    echo UneFonction(2);
+
+
+//
+//
+// Ajouter des commentaires pour dire ce que retournent les appels de fonctions suivants (s'ils renvoient une erreur, marquer "une erreur" et expliquer pourquoi) :
+//
+//echo openBook(6); // test5
+//echo openBook(10); // erreur car il n'y a pas dix éléments dans le tableau
+//echo openBook(5); // un intrus
+
+
+function openBook($page)
+{
+    $tab = [
+        'test',
+        'test2',
+        'test3',
+        'test4',
+        'un intrus',
+        'test5',
+        'test6',
+        'test7',
+        'test8',
+    ];
+    return $tab[$page];
+}
